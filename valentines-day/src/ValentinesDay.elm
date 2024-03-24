810bd6f31046560154d12052efac84c0755781ef
Christian Tejeda <christiantejeda@proton.me>
2024-03-24 15:34:22 -0300

    Valentine's day

---
+++ valentines-day/src/ValentinesDay.elm
@@ -0,0 +1,23 @@
+module ValentinesDay exposing (..)
+
+type Approval = Yes | No | Maybe
+
+type Cuisine = Korean | Turkish
+
+type Genre = Crime | Horror | Romance | Thriller
+
+type Activity =
+    BoardGame |
+    Chill |
+    Movie Genre |
+    Restaurant Cuisine
+
+rateActivity : Activity -> Approval
+rateActivity activity =
+    case activity of
+        BoardGame -> No
+        Chill -> No
+        Movie Romance -> Yes
+        Restaurant Korean -> Yes
+        Restaurant Turkish -> Maybe
+        _ -> No
